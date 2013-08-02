7dbe76cb32bdb84818b4e739557e826ebe773f84
Stefan Petersen <spe@ciellt.se>
2013-08-02 13:39:26 +0200

    Populating DB test implementation.

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,11 +1,17 @@
+class Movie < ActiveRecord::Base
+end
+
 # Add a declarative step here for populating the DB with movies.
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    m = Movie.create!(:title => movie[:title],
+                      :release_date => movie[:release_date],
+                      :rating => movie[:rating])
   end
-  flunk "Unimplemented"
+  #flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
